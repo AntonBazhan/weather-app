561355c426c9670836c9caf8d8d04c51c07ebba7
Anton Bazhan <bazhan.anton@ukr.net>
2021-03-02 14:13:17 +0200

    feat: add weather-app

--- src/components/WeatherItems.jsx
+++ src/components/WeatherItems.jsx
@@ -1,15 +1,13 @@
-// import moment from "moment";
 import WeatherItem from "./WeatherItem";
 import styles from "./weather.module.css";
-
+let n = 0;
 const WeatherItems = ({ items }) => (
   <div className={styles.weatherItems}>
     {items.map(({ dt, temp, weather }) => {
-      let n = 0;
       let date = new Date(Date.now() + 24 * 3600 * 1000 * n);
-      n = +1;
-      const { icon } = weather[0];
 
+      const { icon } = weather[0];
+      n++;
       return <WeatherItem key={dt} date={date} temp={temp} icon={icon} />;
     })}
   </div>
